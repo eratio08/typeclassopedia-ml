7384a9227bac4902022eedfa7618620cae0c0640
Eike Lurz <eike.lurz@tuta.io>
2024-03-20 21:34:56 +0100

    Simplify types in applicative and maybe

--- lib/either.ml
+++ lib/either.ml
@@ -26,36 +26,33 @@ module Applicative (E : sig
     type t
 
     val empty : t
-  end) : Applicative.Applicative with type 'a t = 'a Functor(E).t = struct
-  include Applicative.MakeApplicative (struct
-      include Functor (E)
-      open Either (E)
+  end) : Applicative.Applicative with type 'a t = 'a Functor(E).t =
+Applicative.MakeApplicative (struct
+    include Functor (E)
+    open Either (E)
 
-      let pure a = Right a
-      let empty = Left E.empty
+    let pure a = Right a
+    let empty = Left E.empty
 
-      let ( <*> ) (f : ('a -> 'b) t) (t : 'a t) : 'b t =
-        match f, t with
-        | Right f, Right a -> Right (f a)
-        | Left f, _ -> Left f
-        | _, Left e -> Left e
-      ;;
-    end)
-end
+    let ( <*> ) (f : ('a -> 'b) t) (t : 'a t) : 'b t =
+      match f, t with
+      | Right f, Right a -> Right (f a)
+      | Left f, _ -> Left f
+      | _, Left e -> Left e
+    ;;
+  end)
 
 module Monad (E : sig
     type t
 
     val empty : t
-  end) : Monad.Monad with type 'a t = 'a Applicative(E).t = struct
-  include Monad.MakeMonad (struct
-      include Applicative (E)
-      open Either (E)
+  end) : Monad.Monad with type 'a t = 'a Applicative(E).t = Monad.MakeMonad (struct
+    include Applicative (E)
+    open Either (E)
 
-      let ( >>= ) t f =
-        match t with
-        | Right a -> f a
-        | Left e -> Left e
-      ;;
-    end)
-end
+    let ( >>= ) t f =
+      match t with
+      | Right a -> f a
+      | Left e -> Left e
+    ;;
+  end)
--- lib/maybe.ml
+++ lib/maybe.ml
@@ -2,46 +2,64 @@ module Maybe = struct
   type 'a t =
     | Nothing
     | Just of 'a
+
+  let pp pp_a fmt = function
+    | Nothing -> Format.fprintf fmt "Nothing@."
+    | Just a -> Format.fprintf fmt "Just(%a)@." pp_a a
+  ;;
 end
 
-module Functor : Functor.Functor with type 'a t = 'a Maybe.t = struct
-  include Functor.MakeFunctor (struct
-      include Maybe
+module Functor : Functor.Functor with type 'a t = 'a Maybe.t = Functor.MakeFunctor (struct
+    include Maybe
 
-      let fmap f = function
-        | Nothing -> Nothing
-        | Just a -> Just (f a)
-      ;;
+    let fmap f = function
+      | Nothing -> Nothing
+      | Just a -> Just (f a)
+    ;;
 
-      let ( <$ ) a _ = Just a
-    end)
-end
+    let ( <$ ) a _ = Just a
+  end)
 
-module Applicative : Applicative.Applicative with type 'a t = 'a Functor.t = struct
-  include Applicative.MakeApplicative (struct
-      include Functor
-      open Maybe
+module Applicative : Applicative.Applicative with type 'a t = 'a Functor.t =
+Applicative.MakeApplicative (struct
+    include Functor
+    open Maybe
 
-      let pure a = Just a
-      let empty = Nothing
+    let pure a = Just a
+    let empty = Nothing
 
-      let ( <*> ) ft t =
-        match ft, t with
-        | Just f, Just a -> Just (f a)
-        | _, _ -> Nothing
-      ;;
-    end)
-end
+    let ( <*> ) ft t =
+      match ft, t with
+      | Just f, Just a -> Just (f a)
+      | _, _ -> Nothing
+    ;;
+  end)
 
-module Monad : Monad.Monad with type 'a t = 'a Applicative.t = struct
-  include Monad.MakeMonad (struct
-      include Applicative
-      open Maybe
+module Monad : Monad.Monad with type 'a t = 'a Applicative.t = Monad.MakeMonad (struct
+    include Applicative
+    open Maybe
 
-      let ( >>= ) t f =
-        match t with
-        | Just a -> f a
-        | Nothing -> Nothing
-      ;;
-    end)
-end
+    let ( >>= ) t f =
+      match t with
+      | Just a -> f a
+      | Nothing -> Nothing
+    ;;
+  end)
+
+module Semigroup (T : sig
+    type t
+
+    val ( <> ) : t -> t -> t
+  end) : Semigroup.Semigroup with type t = T.t Maybe.t = Semigroup.MakeSemiGroup (struct
+    include T
+    include Maybe
+
+    type t = T.t Maybe.t
+
+    let ( <> ) a b =
+      match a, b with
+      | Just a, Just b -> Just (a <> b)
+      | Just a, Nothing | Nothing, Just a -> Just a
+      | _, _ -> Nothing
+    ;;
+  end)
